c1cba3665e7147a55b24cd332d9d606561febbba
Safeswipe <team@safeswipe.com.au>
2025-05-05 03:57:27 +1000

    Add files via upload

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,9 +4,8 @@ import Stripe from 'stripe';
 import { createClient } from '@supabase/supabase-js';
 import { buffer } from 'node:stream/consumers';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
